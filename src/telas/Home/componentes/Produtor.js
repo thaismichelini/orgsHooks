14b0c9e85b0f1e71324b7ccd5e2352ff6cc5b498
thaismichelini <thaislmichelini@gmail.com>
2022-07-19 17:19:04 -0300

    componente de estrelas

--- src/telas/Home/componentes/Produtor.js
+++ src/telas/Home/componentes/Produtor.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import {Text, View, Image, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {Text, View, Image, StyleSheet, TouchableOpacity} from 'react-native';
+import Estrelas from '../../../componentes/Estrelas';
 
 export default function Produtor({nome, imagem, distancia, estrelas}) {
+  const [selecionado, setSelecionado] = useState(false);
   return (
-    <View style={estilos.cartao}>
+    <TouchableOpacity
+      style={estilos.cartao}
+      onPress={() => setSelecionado(!selecionado)}>
       <Image source={imagem} style={estilos.imagem} accessibilityLabel={nome} />
       <View style={estilos.informacoes}>
-        <Text style={estilos.nome}>{nome}</Text>
+        <View>
+          <Text style={estilos.nome}>{nome}</Text>
+          <Estrelas
+            quantidade={estrelas}
+            editavel={selecionado}
+            grande={selecionado}
+          />
+        </View>
         <Text style={estilos.distancia}>{distancia}</Text>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 }
 
